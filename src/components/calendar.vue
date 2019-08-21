8af2d2f0093745a63c9c7169e1c981f9e3d812bc
yxp <yuxinpeng@haier.com>
2019-08-21 18:40:19 +0800

    feat：添加单选日期的功能

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,5 +1,17 @@
 <template>
+
   <div class="calendar">
+    <div class="tools">
+      <span class="leftArr"
+        @click='handlePrevM()'>&lt;</span>
+      <div class="center">
+        <span class="year">{{thisY}}</span>
+        <span>-</span>
+        <span class="month">{{thisRM}}</span>
+      </div>
+      <span class="rightArr"
+        @click='handleNextM()'>&gt;</span>
+    </div>
     <div class="weeks">
       <span class="week"
         v-for='(item, index) in weeks'
@@ -8,13 +20,15 @@
     <div class="days">
       <span class="prevMD"
         v-for='(item, index) in prevMDLen'
-        :key='index'>{{monthDays[prevM] - prevLD[thisW] + item}}</span>
+        :key='index + "p"'>{{monthDays[prevM] - prevLD[thisW] + item}}</span>
       <span class="thisMD"
+        :class='{"current": (thisD == index + 1) && isCurM }'
         v-for='(item, index) in thisMDLen'
-        :key='index'>{{index + 1}}</span>
+        :key='index + "c"'
+        @click='handleSelCurD(index)'>{{index + 1}}</span>
       <span class="nextMD"
         v-for='(item, index) in nextMDLen'
-        :key='index'>{{index + 1}}</span>
+        :key='index + "n"'>{{index + 1}}</span>
     </div>
   </div>
 </template>
@@ -30,12 +44,23 @@ export default {
       weeks: '一二三四五六日'.split(''),
       monthDays: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
       prevLD: [6, 0, 1, 2, 3, 4, 5],
-      thisY: new Date().getFullYear(),
-      thisM: new Date().getMonth(),
-      thisD: new Date().getDate(),
+      selDate: new Date(),
+      selDay: new Date(),
     }
   },
   computed: {
+    isCurM() {
+      return this.thisM == this.selDay.getMonth()
+    },
+    thisY() {
+      return this.selDate.getFullYear()
+    },
+    thisM() {
+      return this.selDate.getMonth()
+    },
+    thisD() {
+      return this.selDay.getDate()
+    },
     prevM() {
       return this.thisM - 1 < 0 ? 11 : this.thisM - 1
     },
@@ -58,6 +83,20 @@ export default {
       return 42 - this.thisMDLen - this.prevMDLen
     },
   },
+  methods: {
+    handleNextM() {
+      // debugger
+      var curM = this.thisM + 1 > 11 ? 0 : this.thisM + 1
+      this.selDate = new Date(this.thisY, curM)
+    },
+    handlePrevM() {
+      var curM = this.thisM - 1 < 0 ? 11 : this.thisM - 1
+      this.selDate = new Date(this.thisY, curM)
+    },
+    handleSelCurD(index) {
+      this.selDay = new Date(this.thisY, this.thisM, index + 1)
+    },
+  },
 }
 </script>
 
@@ -66,6 +105,14 @@ export default {
   display: inline-block;
   box-shadow: 1px 1px 6px #eee;
 }
+.weeks {
+  border-bottom: 1px solid #eee;
+}
+.current {
+  background: red;
+  color: #fff;
+}
+.tools,
 .weeks,
 .days {
   width: 280px;
@@ -79,6 +126,17 @@ export default {
   .prevMD,
   .nextMD {
     background: #eee;
+    color: #aaa;
   }
+}
+.tools {
+  justify-content: space-around;
+  // .year {
+  //   font-size: 1em;
+  // }
+  // .month {
+  //   font-size: 0.8em;
+  //   color: #999;
+  // }
 }
 </style>
